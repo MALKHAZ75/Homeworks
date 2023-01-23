8a25a2aa8fb2acc0f8018ae0d4d3b2e073bdc560
Malkhaz Dalalishvili <malkhaz.dalalishvili@socarmidstream.ge>
2023-01-23 11:54:20 +0400

    first commit

---
+++ day 1/Lesson.py
@@ -0,0 +1,8 @@
+name = "Malkhaz"
+surname = "Dalalishvili"
+age = "47"
+height = 181
+studies_programming = True
+
+
+print("I am " + name + " " + surname + " " + str(age) + " " + "years old" + " " + "and I started studying programming at GOA.")
